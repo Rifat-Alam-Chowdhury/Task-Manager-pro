c794a470026288a331056dbd25b9a4edec98b70e
rifat chowdhury <rifatalamtabs2@gmail.com>
2025-02-20 02:58:23 +0600

    firebase deployed

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -9,17 +9,6 @@ function App() {
   const { hello } = useContext(AuthContext);
   console.log(hello);
 
-  const f = async () => {
-    const url = import.meta.env.VITE_URL; // Ensure this is correctly set in your .env file
-    try {
-      const response = await fetch(url); // Fetch data from the API
-      const data = await response.json(); // Convert response to JSON
-      console.log(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-  f();
   return (
     <>
       <section>
